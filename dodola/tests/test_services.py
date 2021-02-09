7f0963765955f36b75258825a9488f45c1790c98
Diana <dgergel@gmail.com>
2021-02-08 17:20:52 -0800

    add testing for QDM

--- dodola/tests/test_services.py
+++ dodola/tests/test_services.py
@@ -2,6 +2,7 @@
 """
 
 import numpy as np
+import pytest
 import xarray as xr
 from dodola.services import bias_correct
 from dodola.repository import FakeRepository
@@ -28,8 +29,8 @@ def _datafactory(x, start_time="1950-01-01"):
     )
     return out
 
-
-def test_bias_correct_basic_call():
+@pytest.mark.parametrize("method", ["QDM", "BCSD"])
+def test_bias_correct_basic_call(method):
     """Simple integration test of bias_correct service"""
     # Setup input data.
     n_years = 10
@@ -43,9 +44,6 @@ def test_bias_correct_basic_call():
     y_train = _datafactory(ts)
     # Yes, we're testing and training on the same data...
     x_test = x_train.copy(deep=True)
-
-    # pick test method
-    method = "BCSD"
 
     output_key = "test_output"
     training_model_key = "x_train"
@@ -76,8 +74,14 @@ def test_bias_correct_basic_call():
     # We can't just test for removal of bias here since quantile mapping
     # and adding in trend are both components of bias correction,
     # so testing head and tail values instead
-    head_vals = np.array([-0.08129293, -0.07613746, -0.0709855, -0.0658377, -0.0606947])
-    tail_vals = np.array([0.0520793, 0.06581804, 0.07096781, 0.07612168, 0.08127902])
+    if method == "BCSD":
+        head_vals = np.array([-0.08129293, -0.07613746, -0.0709855, -0.0658377, -0.0606947])
+        tail_vals = np.array([0.0520793, 0.06581804, 0.07096781, 0.07612168, 0.08127902])
+    elif method == "QDM":
+        head_vals = np.array([4., 4.008609 , 4.0172155, 4.0258169, 4.0344106])
+        tail_vals = np.array([3.9655894, 3.9741831, 3.9827845, 3.991391 , 4.])
+    else: 
+        raise ValueError("this method has not yet been tested")
     np.testing.assert_almost_equal(
         fakestorage.storage[output_key]["fakevariable"].squeeze(drop=True).values[:5],
         head_vals,
