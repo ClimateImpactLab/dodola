3699355254b3fc6a91c61a9da5aabbd0a6b523de
Diana <dgergel@gmail.com>
2021-02-08 17:24:32 -0800

    fix trailing whitespace

--- dodola/tests/test_services.py
+++ dodola/tests/test_services.py
@@ -29,6 +29,7 @@ def _datafactory(x, start_time="1950-01-01"):
     )
     return out
 
+
 @pytest.mark.parametrize("method", ["QDM", "BCSD"])
 def test_bias_correct_basic_call(method):
     """Simple integration test of bias_correct service"""
@@ -75,12 +76,16 @@ def test_bias_correct_basic_call(method):
     # and adding in trend are both components of bias correction,
     # so testing head and tail values instead
     if method == "BCSD":
-        head_vals = np.array([-0.08129293, -0.07613746, -0.0709855, -0.0658377, -0.0606947])
-        tail_vals = np.array([0.0520793, 0.06581804, 0.07096781, 0.07612168, 0.08127902])
+        head_vals = np.array(
+            [-0.08129293, -0.07613746, -0.0709855, -0.0658377, -0.0606947]
+        )
+        tail_vals = np.array(
+            [0.0520793, 0.06581804, 0.07096781, 0.07612168, 0.08127902]
+        )
     elif method == "QDM":
-        head_vals = np.array([4., 4.008609 , 4.0172155, 4.0258169, 4.0344106])
-        tail_vals = np.array([3.9655894, 3.9741831, 3.9827845, 3.991391 , 4.])
-    else: 
+        head_vals = np.array([4.0, 4.008609, 4.0172155, 4.0258169, 4.0344106])
+        tail_vals = np.array([3.9655894, 3.9741831, 3.9827845, 3.991391, 4.0])
+    else:
         raise ValueError("this method has not yet been tested")
     np.testing.assert_almost_equal(
         fakestorage.storage[output_key]["fakevariable"].squeeze(drop=True).values[:5],
