9e95f5f78c9ef61fb28ac8e8f01600405b5fa94c
Brewster Malevich <bmalevich@rhg.com>
2021-02-24 20:40:17 -0800

    Add cli subcommand for rechunker

--- dodola/cli.py
+++ dodola/cli.py
@@ -182,3 +182,94 @@ def buildweights(
         storage=storage,
         outpath=str(outpath),
     )
+
+
+@dodola_cli.command(help="Rechunk Zarr store")
+@click.argument("x", required=True)
+@click.option(
+    "--variable",
+    "-v",
+    required=True,
+    help="Variable to rechunk",
+)
+@click.option(
+    "--chunk",
+    "-c",
+    required=True,
+    help="coord=chunksize to rechunk to",
+)
+@click.option(
+    "--maxmemory",
+    "-m",
+    required=True,
+    help="Max memory (bytes) to use for rechunking",
+)
+@click.option(
+    "--outpath",
+    "-o",
+    required=True,
+)
+@click.option(
+    "--azstorageaccount",
+    default=None,
+    envvar="AZURE_STORAGE_ACCOUNT",
+    help="Key-based Azure storage credential",
+)
+@click.option(
+    "--azstoragekey",
+    default=None,
+    envvar="AZURE_STORAGE_KEY",
+    help="Key-based Azure storage credential",
+)
+@click.option(
+    "--azclientid",
+    default=None,
+    envvar="AZURE_CLIENT_ID",
+    help="Service Principal-based Azure storage credential",
+)
+@click.option(
+    "--azclientsecret",
+    default=None,
+    envvar="AZURE_CLIENT_SECRET",
+    help="Service Principal-based Azure storage credential",
+)
+@click.option(
+    "--aztenantid",
+    default=None,
+    envvar="AZURE_TENANT_ID",
+    help="Service Principal-based Azure storage credential",
+)
+def rechunk(
+    x,
+    variable,
+    chunk,
+    maxmemory,
+    outpath,
+    azstorageaccount,
+    azstoragekey,
+    azclientid,
+    azclientsecret,
+    aztenantid,
+):
+    """Rechunk Zarr store"""
+
+    # Configure storage while we have access to users configurations.
+    storage = adl_repository(
+        account_name=azstorageaccount,
+        account_key=azstoragekey,
+        client_id=azclientid,
+        client_secret=azclientsecret,
+        tenant_id=aztenantid,
+    )
+
+    # Convert ["k1=1", "k2=2"] into {k1: 1, k2: 2}
+    coord_chunks = {c.split("=")[0]: int(c.split("=")[1]) for c in chunk}
+    target_chunks = {variable: coord_chunks}
+
+    services.rechunk(
+        str(x),
+        target_chunks=target_chunks,
+        out=outpath,
+        max_mem=maxmemory,
+        storage=storage,
+    )
