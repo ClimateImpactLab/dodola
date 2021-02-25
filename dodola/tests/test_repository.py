2a2f5cca48c4fb3bcd07f89d2e46f53360618c15
Brewster Malevich <bmalevich@rhg.com>
2021-02-24 20:02:17 -0800

    Fix misleading repo test name

--- dodola/tests/test_repository.py
+++ dodola/tests/test_repository.py
@@ -2,13 +2,13 @@ from xarray import Dataset
 from dodola.repository import memory_repository
 
 
-def test_fakerepository_read():
+def test_memory_repository_read():
     """Basic test that memory_repository.read() works"""
     storage = memory_repository(storage={"foo": Dataset({"bar": 123})})
     assert storage.read(url_or_path="foo") == Dataset({"bar": 123})
 
 
-def test_fakerepository_write():
+def test_memory_repository_write():
     """Basic test that memory_repository.write() works"""
     storage = memory_repository(storage={"foo": Dataset({"bar": 321})})
     storage.write(url_or_path="foo", x=Dataset({"bar": "SPAM"}))
