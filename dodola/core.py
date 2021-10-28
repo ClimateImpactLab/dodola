8bb5bee922be79698c3e14534f2d60f11b494a1f
Diana <dgergel@gmail.com>
2021-10-28 14:48:07 -0700

    update input args for train fctn

--- dodola/core.py
+++ dodola/core.py
@@ -156,8 +156,8 @@ def train_analogdownscaling(
         )
 
     aiqpd = sdba.adjustment.AnalogQuantilePreservingDownscaling.train(
-        coarse_reference[variable],
-        fine_reference[variable],
+        ref=coarse_reference[variable],
+        hist=fine_reference[variable],
         kind=str(kind),
         group=sdba.Grouper("time.dayofyear", window=int(window_n)),
         nquantiles=quantiles_n,
