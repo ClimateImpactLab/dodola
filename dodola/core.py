3fd0c3ae2e2ec1a7c33e1775123d0f6a262a6cbe
Brewster Malevich <bmalevich@rhg.com>
2021-12-09 16:33:01 -0800

    Remove more bias-correction core cruft

--- dodola/core.py
+++ dodola/core.py
@@ -272,57 +272,6 @@ def adjust_analogdownscaling(simulation, qplad, variable):
     return out
 
 
-def apply_bias_correction(
-    gcm_training_ds,
-    obs_training_ds,
-    gcm_predict_ds,
-    train_variable,
-    out_variable,
-    method,
-):
-
-    """Bias correct input model data using specified method,
-       using either monthly or +/- 15 day time grouping. Currently
-       the QDM method is supported.
-
-    Parameters
-    ----------
-    gcm_training_ds : Dataset
-        training model data for building quantile map
-    obs_training_ds : Dataset
-        observation data for building quantile map
-    gcm_predict_ds : Dataset
-        future model data to be bias corrected
-    train_variable : str
-        variable name used in training data
-    out_variable : str
-        variable name used in downscaled output
-    method : {"QDM"}
-        method to be used in the applied bias correction
-
-    Returns
-    -------
-    ds_predicted : xr.Dataset
-        Dataset that has been bias corrected.
-    """
-
-    if method == "QDM":
-        # instantiates a grouper class that groups by day of the year
-        # centered window: +/-15 day group
-        group = sdba.Grouper("time.dayofyear", window=31)
-        model = sdba.adjustment.QuantileDeltaMapping.train(
-            ref=obs_training_ds[train_variable],
-            hist=gcm_training_ds[train_variable],
-            group=group,
-            kind="+",
-        )
-        predicted = model.adjust(sim=gcm_predict_ds[train_variable])
-    else:
-        raise ValueError("this method is not supported")
-    ds_predicted = predicted.to_dataset(name=out_variable)
-    return ds_predicted
-
-
 def _add_cyclic(ds, dim):
     """
     Adds wrap-around, appending first value to end of data for named dimension.
